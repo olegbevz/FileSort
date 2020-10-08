354facd759a3f32ce8bc269158ce65ea0e47954b
olegbevz <olegdb@inbox.ru>
2020-10-08 22:44:42 +0300

    added integration tests project with file sort and copmare test

---
+++ FileSort.IntegrationTests/FileSortTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace FileSort.IntegrationTests
+{
+    [TestFixture]
+    public class FileSortTests
+    {
+        [TestCase("simple.txt", "simple_sorted.txt", "simple_expected.txt", TestName = "ShouldSortSimpleFile")]
+        [TestCase("oneline.txt", "oneline_sorted.txt", "oneline_expected.txt", TestName = "ShouldSortOneLineFile")]
+        [TestCase("empty.txt", "empty_sorted.txt", "empty_expected.txt", TestName = "ShouldSortEmptyFile")]
+        public void ShouldSortFileFromContent(string inputFileName, string outputFileName, string expectedFileName)
+        {
+            var currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var executablePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "FileSort.exe");
+
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = executablePath,
+                Arguments = $"{Path.Combine("Content", inputFileName)} {Path.Combine("Content", outputFileName)}",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                WorkingDirectory = currentDirectory
+            };
+
+            var process = new Process { StartInfo = startInfo };
+            process.Start();
+
+            while (!process.StandardOutput.EndOfStream)
+            {
+                var line = process.StandardOutput.ReadLine();
+                Console.WriteLine(line);
+            }
+
+            process.WaitForExit();
+
+            FileAssert.AreEqual(
+                Path.Combine(currentDirectory, "Content", expectedFileName),
+                Path.Combine(currentDirectory, "Content", outputFileName));
+        }
+    }
+}
