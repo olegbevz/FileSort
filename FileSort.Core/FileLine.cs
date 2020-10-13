9aec197a2d001337c7a73d28b2486fc3e805838f
olegbevz <olegdb@inbox.ru>
2020-10-13 15:43:10 +0300

    FileLine parse from stream optimization

--- FileSort.Core/FileLine.cs
+++ FileSort.Core/FileLine.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.IO;
-using System.Text;
 
 namespace FileSort.Core
 {
     public struct FileLine : IComparable
     {
+        private static int _maxNumberLength = int.MaxValue.ToString().Length;
+        private const int _maxNumberDivTen = int.MaxValue / 10;
+        private const int _maxNumberModTen = int.MaxValue % 10;
+
         public static FileLine None = new FileLine();
         public static FileLine Parse(string data)
         {
@@ -17,11 +20,48 @@ namespace FileSort.Core
 
         public static FileLine Parse(StreamReader streamReader)
         {
-            var streamLine = streamReader.ReadLine();
-            return FileLine.Parse(streamLine);
+            if (TryParse(streamReader, out var entry))
+                return entry;
+
+            throw new ArgumentException($"Failed to parse stream line.");
         }
 
-       
+        public static bool TryParse(StreamReader streamReader, out FileLine fileLine)
+        {
+            fileLine = None;
+            char current;
+            int size = 0;
+            bool readNumber = false;
+            int number = 0;
+
+            if (streamReader.EndOfStream)
+                return false;
+
+            while (!streamReader.EndOfStream && ((current = (char)streamReader.Read()) != '.') && size <= _maxNumberLength)
+            {
+                if (char.IsWhiteSpace(current) && !readNumber) continue;
+                if (!char.IsNumber(current)) return false;
+
+                readNumber = true;
+                int numberPart = (current - '0');
+                if (number == _maxNumberDivTen && numberPart > _maxNumberModTen)
+                    return false;
+
+                number = number * 10 + numberPart;
+                size++;
+            }
+
+            if (streamReader.EndOfStream)
+                return false;
+
+            var name = streamReader.ReadLine().Trim();
+            if (string.IsNullOrEmpty(name))
+                return false;
+
+            fileLine = new FileLine(number, name, sizeof(int) + name.Length);
+
+            return true;
+        }
 
         public static bool TryParse(string data, out FileLine fileLine)
         {
---
+++ FileSort.UnitTests/FileLineTests.cs
@@ -0,0 +1,63 @@
+﻿using FileSort.Core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSort.UnitTests
+{
+    [TestFixture]
+    public class FileLineTests
+    {
+        [TestCase("", TestName = "ShouldNotParseEmptyString")]
+        [TestCase(null, TestName = "ShouldNotParseNullString")]
+        [TestCase(" ", TestName = "ShouldNotParseSpaceString")]
+        [TestCase("1234", TestName = "ShouldNotParseNumber")]
+        [TestCase("Apple", TestName = "ShouldNotParseStringWithoutNumber")]
+        [TestCase("123 Apple", TestName = "ShouldNotParseStringWithoutDot")]
+        [TestCase("2147483648. Apple", TestName = "ShouldNotParseStringWithLargeNumber")]
+        [TestCase("21474836489. Apple", TestName = "ShouldNotParseStringWithExtraLargeNumber")]
+        public void ShouldNotParseIncorrectString(string inputString)
+        {
+            Assert.IsFalse(FileLine.TryParse(inputString, out var fileLine));
+            Assert.AreEqual(FileLine.None, fileLine);
+
+            if (inputString != null)
+            {
+                using (var streamReader = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(inputString))))
+                {
+                    Assert.IsFalse(FileLine.TryParse(streamReader, out fileLine));
+                    Assert.AreEqual(FileLine.None, fileLine);
+                }
+            }
+        }
+
+        [TestCase("1.X", 1, "X", TestName = "ShouldParseShortStringWothoutSpace")]
+        [TestCase("1. X", 1, "X", TestName = "ShouldParseShortStringWithSpace")]
+        [TestCase("2. Banana is yellow", 2, "Banana is yellow", TestName = "ShouldParseSimpleString")]
+        [TestCase("2.  Banana is yellow", 2, "Banana is yellow", TestName = "ShouldParseSimpleStringWithDoubleSpace")]
+        [TestCase("2. Banana is yellow ", 2, "Banana is yellow", TestName = "ShouldParseSimpleStringWithSpaceAtTheEnd")]
+        [TestCase("2. Banana  yellow", 2, "Banana  yellow", TestName = "ShouldParseSimpleStringWithDoubleSpaceInTheMiddle")]
+        [TestCase(" 2. Banana is yellow", 2, "Banana is yellow", TestName = "ShouldParseSimpleStringWithSpaceAtStart")]
+        [TestCase("2147483647. Apple", int.MaxValue, "Apple", TestName = "ShouldNotParseStringWithMaxNumber")]
+        public void ShouldParseCorrectString(string inputString, int number, string name)
+        {
+            Assert.IsTrue(FileLine.TryParse(inputString, out var fileName));
+            Assert.AreEqual(number, fileName.Number);
+            Assert.AreEqual(name, fileName.Name);
+
+            if (inputString != null)
+            {
+                using (var streamReader = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(inputString))))
+                {
+                    Assert.IsTrue(FileLine.TryParse(streamReader, out fileName));
+                    Assert.AreEqual(number, fileName.Number);
+                    Assert.AreEqual(name, fileName.Name);
+                }
+            }
+        }
+    }
+}
