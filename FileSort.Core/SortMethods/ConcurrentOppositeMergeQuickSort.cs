61c080804c2ae18a66939d9408ff09baa700225f
olegbevz <olegdb@inbox.ru>
2020-10-17 15:44:31 +0300

    try catch logic for concurrent sorting

--- FileSort.Core/SortMethods/ConcurrentOppositeMergeQuickSort.cs
+++ FileSort.Core/SortMethods/ConcurrentOppositeMergeQuickSort.cs
@@ -38,7 +38,7 @@ namespace FileSort.Core
             var sortTasks = new Task[_concurrency];
             for (int i = 0; i < _concurrency; i++)
             {
-                sortTasks[i] = Task.Run(async () => await SortChunks(readChannel.Reader, sortChannel.Writer).ConfigureAwait(false));
+                sortTasks[i] = Task.Run(async () => await SortChunks(readChannel.Reader, sortChannel.Writer));
             }
             var mergeTask = Task.Run(async () => await PushChunksToStackAndMerge(sortChannel.Reader).ConfigureAwait(false));
 
@@ -71,72 +71,98 @@ namespace FileSort.Core
 
         private async Task ReadChunks(IEnumerable<T> source, ChannelWriter<List<T>> channelWriter)
         {
-            _logger.Info("Starting reading phase...");
-
-            var currentChunk = new List<T>();
-            long currentChunkSize = 0;
-
-            foreach (var value in source)
+            try
             {
-                currentChunk.Add(value);
-                if (currentChunkSize < _chunkSize)
+                _logger.Info("Starting reading phase...");
+
+                var currentChunk = new List<T>();
+                long currentChunkSize = 0;
+
+                foreach (var value in source)
                 {
-                    currentChunkSize++;
+                    currentChunk.Add(value);
+                    if (currentChunkSize < _chunkSize)
+                    {
+                        currentChunkSize++;
+                    }
+                    else
+                    {
+                        _logger.Debug($"Chunk readen from input source");
+                        await channelWriter.WriteAsync(currentChunk).ConfigureAwait(false);
+                        _logger.Debug($"Chunk pushed to channel");
+                        currentChunk = new List<T>();
+                        currentChunkSize = 0;
+                    }
                 }
-                else
-                {
-                    _logger.Debug($"Chunk readen from input source");
-                    await channelWriter.WriteAsync(currentChunk).ConfigureAwait(false);
-                    _logger.Debug($"Chunk pushed to channel");
-                    currentChunk = new List<T>();
-                    currentChunkSize = 0;
-                }
+
+                _logger.Debug($"Chunk readen from input source");
+                await channelWriter.WriteAsync(currentChunk).ConfigureAwait(false);
+                _logger.Debug($"Chunk pushed to channel");
+
+                channelWriter.Complete();
+                _logger.Info("Reading phase is completed.");
             }
-
-            _logger.Debug($"Chunk readen from input source");
-            await channelWriter.WriteAsync(currentChunk).ConfigureAwait(false);
-            _logger.Debug($"Chunk pushed to channel");
-
-            channelWriter.Complete();
-            _logger.Info("Reading phase is completed.");
+            catch (Exception ex)
+            {
+                _logger.ErrorException("Error occured while reading: ", ex);
+                channelWriter.Complete(ex);
+                throw;
+            }
         }        
 
         private async Task SortChunks(ChannelReader<List<T>> channelReader, ChannelWriter<List<T>> channelWriter)
         {
-            int index = Interlocked.Increment(ref _concurrencyCounter);
-
-            _logger.Info($"{index}. Started sorting phase...");
-
-            while (await channelReader.WaitToReadAsync().ConfigureAwait(false))
+            try
             {
-                if (channelReader.TryRead(out var chunk))
-                {
-                    _logger.Debug($"{index}. Chunk sorting");
-                    chunk.Sort();
-                    _logger.Debug($"{index}. Chunk sorted");
-                    await channelWriter.WriteAsync(chunk).ConfigureAwait(false);
-                }
-            }
+                int index = Interlocked.Increment(ref _concurrencyCounter);
 
-            _logger.Info($"{index}. Sorting phase is completed.");
+                _logger.Info($"{index}. Started sorting phase...");
+
+                while (await channelReader.WaitToReadAsync().ConfigureAwait(false))
+                {
+                    if (channelReader.TryRead(out var chunk))
+                    {
+                        _logger.Debug($"{index}. Chunk sorting");
+                        chunk.Sort();
+                        _logger.Debug($"{index}. Chunk sorted");
+                        await channelWriter.WriteAsync(chunk).ConfigureAwait(false);
+                    }
+                }
+
+                _logger.Info($"{index}. Sorting phase is completed.");
+            }
+            catch (Exception ex)
+            {
+                _logger.ErrorException("Error occured while sorting: ", ex);
+                channelWriter.Complete(ex);
+                throw;
+            }
         }
 
         private async Task<IChunkReference<T>> PushChunksToStackAndMerge(ChannelReader<List<T>> channelReader)
         {
-            while (await channelReader.WaitToReadAsync().ConfigureAwait(false))
+            try
             {
-                if (channelReader.TryRead(out var chunk))
+                while (await channelReader.WaitToReadAsync().ConfigureAwait(false))
                 {
-                    _chunkStack.Push(chunk);
+                    if (channelReader.TryRead(out var chunk))
+                    {
+                        _chunkStack.Push(chunk);
+                    }
                 }
+
+                if (_chunkStack.Count == 0)
+                    return null;
+
+                _logger.Info("Starting final merge");
+
+                return _appender.ExecuteFinalMerge();
             }
-
-            if (_chunkStack.Count == 0)
-                return null;
-
-            _logger.Info("Starting final merge");
-
-            return _appender.ExecuteFinalMerge();
+            catch (Exception ex)
+            {
+                _logger.ErrorException("Error occured while merging: ", ex);
+                throw;
+            }
         }
     }
 }
