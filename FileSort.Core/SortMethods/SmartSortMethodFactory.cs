ee2dc9cfca1ecfadcaa6418213665a1989162a60
Oleg Bevz <olegdb@inbox.ru>
2020-10-19 18:53:27 +0300

    Update SmartSortMethodFactory.cs

--- FileSort.Core/SortMethods/SmartSortMethodFactory.cs
+++ FileSort.Core/SortMethods/SmartSortMethodFactory.cs
@@ -3,80 +3,29 @@ using System;
 namespace FileSort.Core
 {
     /// <summary>
-    /// SortMethodFactory represents factory for a sort nethod logic
+    /// SmartSortMethodFactory represents factory for a sort nethod logic
+    /// SortMethod here is determined based on a file size
     /// </summary>
-    public class SortMethodFactory : ISortMethodFactory
+    public class SmartSortMethodFactory : SortMethodFactory
     {
-        private readonly int _channelCapacity;
-        private readonly int _sortConcurrency;
-        private readonly int _mergeConcurrency;
-        private readonly int? _quickSortSize;
-        private readonly bool _onlyMemoryMerge;
-        private readonly SortMethod _sortMethod;
-
-        public SortMethodFactory(
-            SortMethod sortMethod,
-            int channelCapacity, 
-            int sortConcurrency, 
-            int mergeConcurrency,
-            bool onlyMemoryMerge,
-            int? quickSortSize)
+        public SmartSortMethodFactory(long fileSize, int channelCapacity, int sortConcurrency, int mergeConcurrency, bool onlyMemoryMerge, int? quickSortSize)
+            : base(CalculateSortMethod(fileSize), channelCapacity, sortConcurrency, mergeConcurrency, onlyMemoryMerge, quickSortSize)
         {
-            _sortMethod = sortMethod;
-            _channelCapacity = channelCapacity;
-            _sortConcurrency = sortConcurrency;
-            _mergeConcurrency = mergeConcurrency;
-            _onlyMemoryMerge = onlyMemoryMerge;
-            _quickSortSize = quickSortSize;
         }
 
-        public ISortMethod<T> CreateSortMethod<T>(ChunkStack<T> chunkStack, IChunkStackFactory<T> chunkStackFactory) where T : IComparable
+        private static SortMethod CalculateSortMethod(long fileSize)
         {
-            switch (_sortMethod)
+            if (fileSize <= MemorySize.KB)
             {
-                case SortMethod.MergeSort:
-                    {
-                        return new OppositeMergeSort<T>(
-                            chunkStack, 
-                            chunkStackFactory.CreateChunkStack(), 
-                            _onlyMemoryMerge);
-                    }
-                case SortMethod.MergeQuickSort:
-                    {
-                        if (_quickSortSize != null)
-                            return new OppositeMergeQuickSort<T>(
-                                chunkStack,
-                                chunkStackFactory.CreateChunkStack(), 
-                                _quickSortSize.Value,
-                                _onlyMemoryMerge);
-
-                        return new OppositeMergeQuickSort<T>(
-                            chunkStack,
-                            chunkStackFactory.CreateChunkStack(),
-                            onlyMemoryMerge: _onlyMemoryMerge);
-                    }
-                case SortMethod.ConcurrentMergeQuickSort:
-                    {
-                        if (_quickSortSize != null)
-                            return new ConcurrentOppositeMergeQuickSort<T>(
-                                chunkStack,
-                                chunkStackFactory,
-                                _channelCapacity,
-                                _sortConcurrency,
-                                _mergeConcurrency,
-                                _quickSortSize.Value,
-                                _onlyMemoryMerge);
-
-                        return new ConcurrentOppositeMergeQuickSort<T>(
-                            chunkStack,
-                            chunkStackFactory,
-                            _channelCapacity,
-                            _sortConcurrency,
-                            _mergeConcurrency,
-                            onlyMemoryMerge: _onlyMemoryMerge);
-                    }
-                default:
-                    throw new NotSupportedException();
+                return SortMethod.MergeSort;
+            }
+            else if (fileSize <= 10 * MemorySize.MB)
+            {
+                return SortMethod.MergeQuickSort;
+            }
+            else
+            {
+                return SortMethod.ConcurrentMergeQuickSort;
             }
         }
     }
